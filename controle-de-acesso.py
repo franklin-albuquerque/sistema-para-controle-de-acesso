3411952f02f488c218c4502d915b59c9ef5cb4ae
Franklin Albuquerque <126638424+franklin-albuquerque@users.noreply.github.com>
2023-04-25 17:30:39 -0300

    Adiciona integração com o PostgreSQL

--- controle-de-acesso.py
+++ controle-de-acesso.py
@@ -1,55 +1,125 @@
 from ast import literal_eval
 from bcrypt import checkpw, hashpw, gensalt
 from os import path
+from psycopg2 import connect, DatabaseError
+from dotenv import dotenv_values
+
+def criptografar(senha):
+    return hashpw(senha.encode('utf-8'), gensalt(13))
+
+def criar_bd():
+    conexao = conectar_bd()
+    cursor = conexao.cursor()
+
+    sql = '''CREATE TABLE IF NOT EXISTS usuarios (
+        id SERIAL PRIMARY KEY,
+        nome TEXT NOT NULL,
+        email TEXT NOT NULL,
+        senha TEXT NOT NULL
+    )'''
+
+    cursor.execute(sql)
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+
+def conectar_bd():
+    conexao = None
+
+    if path.exists('.env'):
+        config = dotenv_values('.env', encoding='utf-16')
+        conexao = connect(host=config['host'],
+                          database=config['banco_de_dados'],
+                          user=config['usuario'],
+                          password=config['senha'])
+    else:
+        print('Não foi possível conectar ao banco de dados')
+
+    return conexao
+
+def inserir_bd(nome, email, senha):
+    conexao = conectar_bd()
+    cursor = conexao.cursor()
+
+    try:
+        sql = f"INSERT INTO usuarios (nome, email, senha) VALUES ('{nome}', '{email}', '{senha}')"
+        cursor.execute(sql)
+        conexao.commit()
+        print('Usuário registrado com sucesso')
+    except (Exception, DatabaseError) as erro:
+        print(f'Erro: {erro}')
+        conexao.rollback()
+    finally:
+        cursor.close()
+        conexao.close()
 
 def acessar_sistema():
     email = input('Digite seu endereço de e-mail: ')
     senha = input('Digite sua senha: ')
 
+    conexao = conectar_bd()
+    cursor = conexao.cursor()
+
     try:
-        with open('banco_de_dados.db', 'r') as arquivo:
-            for linha in arquivo:
-                usuario = literal_eval(linha)
-                for email_, senha_ in usuario.items():
-                    if email_ == email:
-                        if checkpw(senha.encode('utf-8'), senha_):
-                            print('Acesso autorizado')
-                        else:
-                            print('Acesso negado')
-                        return
-            else:
-                print('Não há nenhuma conta associada ao endereço de e-mail')
+        consulta = "SELECT email, senha FROM usuarios"
+        cursor.execute(consulta)
+        linhas = cursor.fetchall()
 
-    except FileNotFoundError:
-        print('Banco de dados não encontrado')
+        for linha in linhas:
+            email_ = linha[0]
+            senha_ = linha[1]
 
-def criptografar(senha):
-    return hashpw(senha.encode('utf-8'), gensalt(13))
+            if email_ == email:
+                if checkpw(senha.encode('utf-8'), senha_.encode('utf-8')):
+                    print('Acesso autorizado')
+                else:
+                    print('Senha incorreta')
+                break
+        else:
+            print('Seu endereço de e-mail não está registrado no sistema')
+    except:
+        print('Não foi possível acessar o banco de dados')
+    finally:
+        cursor.close()
+        conexao.close()
+
+def consultar_bd(email):
+    resultado = None
+
+    conexao = conectar_bd()
+    cursor = conexao.cursor()
+
+    try:
+        consulta = "SELECT email FROM usuarios"
+        cursor.execute(consulta)
+        linhas = cursor.fetchall()
+
+        for linha in linhas:
+            email_ = linha[0]
+
+            if email_ == email:
+                resultado = True
+                break
+    finally:
+        cursor.close()
+        conexao.close()
+
+    return resultado
 
 def cadastrar_usuario():
+    nome = input('Digite seu nome: ')
     email = input('Digite seu endereço de e-mail: ')
-    senha = criptografar(input('Digite sua senha: '))
+    senha = criptografar(input('Digite sua senha: ')).decode('utf-8')
 
-    usuario = {email: senha}
+    try:
+        criar_bd()
 
-    if path.exists('banco_de_dados.db'):
-        with open('banco_de_dados.db', 'r') as arquivo:
-            for linha in arquivo:
-                usuario_banco = literal_eval(linha)
-
-                for email_, _ in usuario_banco.items():
-                    if email_ == email:
-                        print('Já existe uma conta associada ao e-mail informado')
-                        return
-            else:
-                with open('banco_de_dados.db', 'a+') as arquivo:
-                    arquivo.write(f'{usuario}\n')
-                print('Cadastrado com sucesso', end='\n')
-                return
-    else:
-        with open('banco_de_dados.db', 'a+') as arquivo:
-            arquivo.write(f'{usuario}\n')
-        print('Cadastrado com sucesso', end='\n')
+        if consultar_bd(email):
+            print('O usuário já está cadastrado no sistema')
+        else:
+            inserir_bd(nome, email, senha)
+    except:
+        print('Erro no cadastramento')
 
 def opcoes(entrada):
     if entrada == 1:
